e878441c1c8538d9f6f29b1e019db5f1f30bab50
Adalberto Junior <adalrsjr1@gmail.com>
2022-12-27 18:30:56 -0500

    add deployment files

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -37,13 +37,12 @@ func main() {
 
 	go tb.StartAffinityInformer(informersCtx, db)
 	go tb.StartNodeAffinityInformer(informersCtx, db)
-	// go tb.StartNodeMetricsInformer(informersCtx, db)
-	// go tb.StartPodMetricsInformer(informersCtx, db)
-	// go tb.StartPodInformer(informersCtx, db)
-	// // // TEST pod x endpoint: SELECT Pods.name AS pod, Endpoints.name AS endpoint, Pods.ip AS pod_ip , Endpoints.ip AS endpoint_IP FROM Pods LEFT JOIN Endpoints ON Pods.ip=Endpoints.ip ORDER BY Pods.name, Endpoints.name ASC
-	// go tb.StartEndpointInformer(informersCtx, db)
-	// go tb.StartNodeInformer(informersCtx, db)
-	// go tb.StartContainerInformer(informersCtx, db)
+	go tb.StartNodeMetricsInformer(informersCtx, db)
+	go tb.StartPodMetricsInformer(informersCtx, db)
+	go tb.StartPodInformer(informersCtx, db)
+	go tb.StartEndpointInformer(informersCtx, db)
+	go tb.StartNodeInformer(informersCtx, db)
+	go tb.StartContainerInformer(informersCtx, db)
 	config := server.Config{
 		Protocol: "tcp",
 		Address:  fmt.Sprintf("%s:%d", address, port),
@@ -56,23 +55,3 @@ func main() {
 		log.Error(err)
 	}
 }
-
-// func createTestDatabase(ctx *sql.Context) *memory.Database {
-// 	// mysql --host=0.0.0.0 --port=3306 --user=root mydb --execute="SELECT * FROM mytable;"
-// 	db := memory.NewDatabase(dbName)
-// 	table := memory.NewTable(tableName, sql.NewPrimaryKeySchema(sql.Schema{
-// 		{Name: "name", Type: sql.Text, Nullable: false, Source: tableName, PrimaryKey: true},
-// 		{Name: "email", Type: sql.Text, Nullable: false, Source: tableName, PrimaryKey: true},
-// 		{Name: "phone_numbers", Type: sql.JSON, Nullable: false, Source: tableName},
-// 		{Name: "created_at", Type: sql.Datetime, Nullable: false, Source: tableName},
-// 	}), db.GetForeignKeyCollection())
-// 	db.AddTable(tableName, table)
-
-// 	creationTime := time.Unix(0, 1667304000000001000).UTC()
-// 	_ = table.Insert(ctx, sql.NewRow("Jane Deo", "janedeo@gmail.com", sql.MustJSON(`["556-565-566", "777-777-777"]`), creationTime))
-// 	_ = table.Insert(ctx, sql.NewRow("Jane Doe", "jane@doe.com", sql.MustJSON(`[]`), creationTime))
-// 	_ = table.Insert(ctx, sql.NewRow("John Doe", "john@doe.com", sql.MustJSON(`["555-555-555"]`), creationTime))
-// 	_ = table.Insert(ctx, sql.NewRow("John Doe", "johnalt@doe.com", sql.MustJSON(`[]`), creationTime))
-
-// 	return db
-// }
